5115a73f0118cb016e1525cb10bf032eb81f75e1
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-19 23:12:12 +0545

    oush

--- Searching/Another.java
+++ Searching/Another.java
@@ -2,6 +2,7 @@ public class Another {
     public static void main(String[] args) {
         int [] num={1,2,3,4,5,6,7,8,91,23,24,53};
         int chosen=91;
+        System.out.println("Why not updated");
         int result=search(num, chosen);
         System.out.println("Item in index : "+result);
     }
