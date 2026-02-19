c3476e461b7ad0dcc174bd5d57e5568764b030b3
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-19 23:08:29 +0545

    well removed some space complexity

---
+++ Searching/Another.java
@@ -0,0 +1,20 @@
+public class Another {
+    public static void main(String[] args) {
+        int [] num={1,2,3,4,5,6,7,8,91,23,24,53};
+        int chosen=91;
+        int result=search(num, chosen);
+        System.out.println("Item in index : "+result);
+    }
+
+    static int search(int [] arr, int target){
+        if(arr.length==0){
+            return -1;
+        }
+        for(int i=0; i<arr.length; i++){
+            if(arr[i]==target){
+                return i;
+            }
+        }
+                return -1;
+    }
+}
