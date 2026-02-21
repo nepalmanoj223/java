67310ea05490e8bff03dfbd7190c0faeb4dfb5f6
Manoj Nepal <nepalmanoj223@gmail.com>
2026-02-21 12:20:28 +0545

    Searched performed in Multi dimensional arrays

---
+++ Searching/Twosearch.java
@@ -0,0 +1,27 @@
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
+public class Twosearch {
+    public static void main(String[] args){
+        int[][] arr={
+            {21,45,67},
+            {19,28,56},
+            {1,2,5,96,12},
+            {21,13,19}
+        };
+        int target=13;
+        int []ans= search(arr, target);
+        System.out.println(Arrays.toString(ans));
+
+    }
+    static int[] search(int[][] arr, int tareget){
+        for(int i=0; i<arr.length; i++){
+            for(int j=0; j<arr[i].length; j++){
+                if(arr[i][j]==tareget){
+                    return new int[]{i, j};      
+                  }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+}
